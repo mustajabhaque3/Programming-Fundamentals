cdab37bda6afa107602f53613de5750e026cfb4b
mustajabhaque3 <mustajabhaque3@gmail.com>
2026-02-21 10:13:46 +0500

    Add files via upload

---
+++ gun.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+main()
+{
+ system("color 03");
+
+	cout <<"  +--^----------,--------,-----,--------^-,         "<<endl;
+	cout <<"   | |||||||||   '--------'     |          o        "<<endl;
+	cout <<"   '+---------------------------^----------|        "<<endl;
+	cout <<"     ',---------,---------,--------------'          "<<endl;
+	cout <<"       / XXXXXX /'|       /'                        "<<endl;
+	cout <<"      / XXXXXX /  |      /'                         "<<endl;
+	cout <<"     / XXXXXX /'-------'                            "<<endl;
+	cout <<"    / XXXXXX /                                      "<<endl;
+	cout <<"   / XXXXXX /                                       "<<endl;
+	cout <<"  (--------(                                        "<<endl;
+	cout <<"   '------'                                         "<<endl;
+}
---
+++ pikachu.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+main()
+{
+ system("color 70");
+
+	cout <<"  '._                               "<<endl;
+	cout <<"   ' `          ---'                "<<endl;
+	cout <<"    '.'!_...._/'.-''                "<<endl;
+	cout <<"     !         /      '             "<<endl;
+	cout <<"     /()    () !    .' '-._         "<<endl;
+	cout <<"    |)   .    ()!  /   _.'          "<<endl;
+	cout <<"     |   -'-     ,; '. <            "<<endl;
+	cout <<"     ;.__      ,;|   > |            "<<endl;
+	cout <<"    / ,     / ,  |.-'.-'            "<<endl;
+	cout <<"   (_/     (_/ ,;|.<'               "<<endl;
+	cout <<"     !     ,      ;-'               "<<endl;
+	cout <<"      >    !     /                  "<<endl;
+	cout <<"     (_,-''> .'                     "<<endl;
+	cout <<"       (_,'                         "<<endl;
+}
